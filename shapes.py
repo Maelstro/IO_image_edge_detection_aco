497c2b992232c48d73efa3de9eb9aab2bc8e50bf
awojasinski <a.wojasinski@gmail.com>
2021-03-21 13:38:54 +0100

    changed to class, added random facecolor generator

--- shapes.py
+++ shapes.py
@@ -1,132 +1,22 @@
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+import colorsys
 import numpy as np
 from numpy import cos, sin, sqrt, pi, cosh, sinh, exp
 
-def torus():
-    u, v = np.linspace(0, 2*pi, 40), np.linspace(0, 2*pi, 40)
-    u, v = np.meshgrid(u, v)
-    x = (9 + 3*cos(u))*cos(v)
-    y = (9 + 3*cos(u))*sin(v)
-    z = 3*sin(u)
-    
-    fig = plt.figure()
-    ax = fig.gca(projection = '3d')
-    plot = ax.plot_surface(x, y, z, rstride = 1, cstride = 1, cmap = plt.get_cmap('jet'), linewidth = 0, antialiased = False)
-    ax.set_zlim(-10, 10)
-    plt.show()
 
-def sphere():
-    u, v = np.linspace(-pi/2, pi/2, 40), np.linspace(0, 2*pi, 40)
-    u, v = np.meshgrid(u, v)
-    x = 9*cos(u)*cos(v)
-    y = 9*cos(u)*sin(v)
-    z = 9*sin(u)
-    
-    fig = plt.figure()
-    ax = fig.gca(projection = '3d')
-    plot = ax.plot_surface(x, y, z, rstride = 1, cstride = 1, cmap = plt.get_cmap('jet'), linewidth = 0, antialiased = False)
-    ax.set_zlim(-10, 10)
-    plt.show()
+class Shape():
+    def __init__(self, resolution:int, type:str, paramA=10, paramB=5):
+        self.resolution = resolution                    #resolution of mesh
+        self.type = type                                #type of shape
+        self.paramA, self.paramB = paramA, paramB       #parameters used to generate shape
+        self.x, self.y, self.z = self.__get_figure()    #points of shape
 
-def cone():
-    h = 10
-    u, v = np.linspace(0, 2*pi, 40), np.linspace(0, h, 40)
-    u, v = np.meshgrid(u, v)
-    x = (h-v)/h*10*cos(u)
-    y = (h-v)/h*10*sin(u)
-    z = v
-    
-    fig = plt.figure()
-    ax = fig.gca(projection = '3d')
-    plot = ax.plot_surface(x, y, z, rstride = 1, cstride = 1, cmap = plt.get_cmap('jet'), linewidth = 0, antialiased = False)
-    ax.set_zlim(-10, 10)
-    plt.show()
 
-def gaussian():
-    sigmaX = 5
-    sigmaY = 5
-    
-    u, v, t = np.linspace(-10, 10, 40), np.linspace(-10, 10, 40), np.linspace(0, pi, 40)
-    x, y = np.meshgrid(u, v)
-    a = cos(t)**2/(2*sigmaX**2) + sin(t)**2/(2*sigmaY**2)
-    b = -sin(2*t)/(4*sigmaX**2) + sin(2*t)/(4*sigmaY**2)
-    c = sin(t)**2/(2*sigmaX**2) + cos(t)**2/(2*sigmaY**2)
-
-    z = 10*exp( - (a*(x**2) + 2*b*x*y + c*(y**2)))
-    
-    fig = plt.figure()
-    ax = fig.gca(projection = '3d')
-    plot = ax.plot_surface(x, y, z, rstride = 1, cstride = 1, cmap = plt.get_cmap('jet'), linewidth = 0, antialiased = False)
-    ax.set_zlim(-10, 10)
-    plt.show()
-
-def cylinder():
-    h = 10
-    u, v = np.linspace(0, 2*pi, 40), np.linspace(0, h, 40)
-    u, v = np.meshgrid(u, v)
-    x = 10*cos(u)
-    y = 10*sin(u)
-    z = v
-    
-    fig = plt.figure()
-    ax = fig.gca(projection = '3d')
-    plot = ax.plot_surface(x, y, z, rstride = 1, cstride = 1, cmap = plt.get_cmap('jet'), linewidth = 0, antialiased = False)
-    ax.set_zlim(-10, 10)
-    plt.show()
-
-def ripple():
-    pass
-
-def bumps():
-    pass
-
-def shell():
-    u, v = np.linspace(0, 2*pi, 40), np.linspace(-2*pi, 2*pi, 80)
-    u, v = np.meshgrid(u, v)
-    x = 5/4*(1 - (v/(2*pi)))*cos(2*v)*(1+cos(u))+cos(2*v)
-    y = 5/4*(1 - (v/(2*pi)))*sin(2*v)*(1+cos(u))+sin(2*v)
-    z = (10*v)/(2*pi)+5/4*(1 - (v/(2*pi)))*sin(u)+15
-    
-    fig = plt.figure()
-    ax = fig.gca(projection = '3d')
-    plot = ax.plot_surface(x, y, z, rstride = 1, cstride = 1, cmap = plt.get_cmap('jet'), linewidth = 0, antialiased = False)
-    plt.show()
-
-def bottle():
-    u, v = np.linspace(0, 2*pi, 40), np.linspace(0, 2*pi, 40)
-    u, v = np.meshgrid(u, v)
-    half = (0 <= u) & (u < pi)
-    r = 4*(1 - cos(u)/2)
-    x = 6*cos(u)*(1 + sin(u)) + r*cos(v + pi)
-    x[half] = ((6*cos(u)*(1 + sin(u)) + r*cos(u)*cos(v))[half])
-    y = 16 * sin(u)
-    y[half] = (16*sin(u) + r*sin(u)*cos(v))[half]
-    z = r * sin(v)
-    
-    fig = plt.figure()
-    ax = fig.gca(projection = '3d')
-    plot = ax.plot_surface(x, y, z, rstride = 1, cstride = 1, cmap = plt.get_cmap('jet'), linewidth = 0, antialiased = False)
-    plt.show()
-
-def breather():
-    a = 2/5
-    u, v = np.linspace(-14, 14, 80), np.linspace(-37.4, 37.4, 80)
-    u, v = np.meshgrid(u, v)
-    x = -u +(2*(1-a**2)*cosh(a*u)*sinh(a*u))/(a*((1-a**2)*cosh(a*u)**2 + a**2*sin(sqrt(1-a**2)*v)**2))
-    y = (2*sqrt(1-a**2)*cosh(a*u)*(-sqrt(1-a**2)*cos(v)*cos(sqrt(1-a**2)*v)-sin(v)*sin(sqrt(1-a**2)*v)))/(a*((1-a**2)*cosh(a*u)**2 + a**2*sin(sqrt(1-a**2)*v)**2))
-    z = (2*sqrt(1-a**2)*cosh(a*u)*(-sqrt(1-a**2)*sin(v)*cos(sqrt(1-a**2)*v)+cos(v)*sin(sqrt(1-a**2)*v)))/(a*((1-a**2)*cosh(a*u)**2 + a**2*sin(sqrt(1-a**2)*v)**2))
-
-    fig = plt.figure()
-    ax = fig.gca(projection = '3d')
-    plot = ax.plot_surface(x, y, z, rstride = 1, cstride = 1, cmap = plt.get_cmap('jet'), linewidth = 0, antialiased = False)
-    plt.show()
-
-def sinewave():
-    pass
-
-def get_figure(argument):
-    # Dictionary connectiong strings with functions generating shapes
-    switcher = {
+    def __get_figure(self):
+        # Dictionary connectiong strings with functions generating shapes
+        default = torus
+        switcher = {
         "torus": torus,
         "sphere": sphere,
         "cone": cone,
@@ -136,9 +26,140 @@ def get_figure(argument):
         "bumps": bumps,
         "shell": shell,
         "bottle": bottle,
-        "breather": breather,
-        "sinewave": sinewave
+        "breather": breather
 
-    }
-    func = switcher.get(argument)
-    return func()
+        }
+        
+        return switcher.get(self.type, lambda: default)(self)
+
+
+    def __random_facecolors(self):
+        randFaceColors = np.empty(self.x.shape, dtype=tuple)
+
+        for y in range(len(self.y)):
+            for x in range(len(self.x)):
+                randHSV = (np.random.uniform(low=0.0, high=1),
+                                np.random.uniform(low=0.2, high=1),
+                                np.random.uniform(low=0.9, high=1))
+                randFaceColors[x, y] = colorsys.hsv_to_rgb(randHSV[0], randHSV[1], randHSV[2])+(1,)
+
+        return randFaceColors
+
+
+    def get_image(self, height:int, width:int, with_cmap=False):
+        # Returns Image of the shape
+        fig = plt.figure()
+        ax = fig.gca(projection = '3d')
+        if with_cmap:
+            plot = ax.plot_surface(self.x, self.y, self.z, shade=False, facecolors=self.__random_facecolors(), linewidth=0)
+        else:
+            plot = ax.plot_surface(self.x, self.y, self.z, linewidth=0.5, color="white", shade=False, edgecolors="black")
+        ax.set_zlim(-self.paramA, self.paramA)
+        plt.show()
+
+
+def torus(shape:Shape):
+    u, v = np.linspace(0, 2*pi, shape.resolution), np.linspace(0, 2*pi, shape.resolution)
+    u, v = np.meshgrid(u, v)
+    x = (shape.paramA + shape.paramB*cos(u))*cos(v)
+    y = (shape.paramA + shape.paramB*cos(u))*sin(v)
+    z = shape.paramB*sin(u)
+    
+    return x, y, z
+
+
+def sphere(shape:Shape):
+    u, v = np.linspace(-pi/2, pi/2, shape.resolution), np.linspace(0, 2*pi, shape.resolution)
+    u, v = np.meshgrid(u, v)
+    x = shape.paramA*cos(u)*cos(v)
+    y = shape.paramA*cos(u)*sin(v)
+    z = shape.paramA*sin(u)
+    
+    return x, y, z
+
+
+def cone(shape:Shape):
+    h = shape.paramA + shape.paramB
+    u, v = np.linspace(0, 2*pi, shape.resolution), np.linspace(0, h, shape.resolution)
+    u, v = np.meshgrid(u, v)
+    x = (h-v)/h*shape.paramA*cos(u)
+    y = (h-v)/h*shape.paramA*sin(u)
+    z = v
+
+    return x, y, z
+
+
+def gaussian(shape:Shape):
+    sigmaX = shape.paramB/2
+    sigmaY = shape.paramB/2
+    
+    u, t = np.linspace(-shape.paramA, shape.paramA, shape.resolution), np.linspace(0, pi, shape.resolution)
+    x, y = np.meshgrid(u, u)
+    a = cos(t)**2/(2*sigmaX**2) + sin(t)**2/(2*sigmaY**2)
+    b = -sin(2*t)/(4*sigmaX**2) + sin(2*t)/(4*sigmaY**2)
+    c = sin(t)**2/(2*sigmaX**2) + cos(t)**2/(2*sigmaY**2)
+
+    z = shape.paramA*exp( - (a*(x**2) + 2*b*x*y + c*(y**2)))
+    
+    return x, y, z
+
+
+def cylinder(shape:Shape):
+    u, v = np.linspace(0, 2*pi, shape.resolution), np.linspace(0, shape.paramA, shape.resolution)
+    u, v = np.meshgrid(u, v)
+    x = shape.paramA*cos(u)
+    y = shape.paramA*sin(u)
+    z = v
+    
+    return x, y, z
+
+
+def ripple(shape:Shape):    
+    u = np.linspace(-shape.paramA, shape.paramA, shape.resolution)
+    x, y = np.meshgrid(u, u)
+    z = sin((x**2+y**2)/shape.paramA)
+    
+    return x, y, z
+
+
+def bumps(shape:Shape):
+    u = np.linspace(-shape.paramA, shape.paramA, shape.resolution)
+    x, y = np.meshgrid(u, u)
+    z = shape.paramB*sin(x/2)*cos(y/2)/2
+    
+    return x, y, z
+
+
+def shell(shape:Shape):
+    u, v = np.linspace(0, 2*pi, shape.resolution), np.linspace(-2*pi, 2*pi, 2*shape.resolution)
+    u, v = np.meshgrid(u, v)
+    x = 5/4*(1 - (v/(2*pi)))*cos(2*v)*(1+cos(u))+cos(2*v)
+    y = 5/4*(1 - (v/(2*pi)))*sin(2*v)*(1+cos(u))+sin(2*v)
+    z = (10*v)/(2*pi)+5/4*(1 - (v/(2*pi)))*sin(u)+15
+    
+    return x, y, z
+
+
+def bottle(shape:Shape):
+    u, v = np.linspace(0, 2*pi, shape.resolution), np.linspace(0, 2*pi, shape.resolution)
+    u, v = np.meshgrid(u, v)
+    half = (0 <= u) & (u < pi)
+    r = 4*(1 - cos(u)/2)
+    x = 6*cos(u)*(1 + sin(u)) + r*cos(v + pi)
+    x[half] = ((6*cos(u)*(1 + sin(u)) + r*cos(u)*cos(v))[half])
+    y = 16 * sin(u)
+    y[half] = (16*sin(u) + r*sin(u)*cos(v))[half]
+    z = r * sin(v)
+    
+    return x, y, z
+
+
+def breather(shape:Shape):
+    a = 2/5
+    u, v = np.linspace(-14, 14, 2*shape.resolution), np.linspace(-37.4, 37.4, 2*shape.resolution)
+    u, v = np.meshgrid(u, v)
+    x = -u +(2*(1-a**2)*cosh(a*u)*sinh(a*u))/(a*((1-a**2)*cosh(a*u)**2 + a**2*sin(sqrt(1-a**2)*v)**2))
+    y = (2*sqrt(1-a**2)*cosh(a*u)*(-sqrt(1-a**2)*cos(v)*cos(sqrt(1-a**2)*v)-sin(v)*sin(sqrt(1-a**2)*v)))/(a*((1-a**2)*cosh(a*u)**2 + a**2*sin(sqrt(1-a**2)*v)**2))
+    z = (2*sqrt(1-a**2)*cosh(a*u)*(-sqrt(1-a**2)*sin(v)*cos(sqrt(1-a**2)*v)+cos(v)*sin(sqrt(1-a**2)*v)))/(a*((1-a**2)*cosh(a*u)**2 + a**2*sin(sqrt(1-a**2)*v)**2))
+
+    return x, y, z
